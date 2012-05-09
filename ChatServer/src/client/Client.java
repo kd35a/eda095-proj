c643b7275a86faf1eb6e0eca3de981b95ceffacb
Fredrik Appelros <dt07fa4@student.lth.se>
2012-05-09 13:02:01 +0200

    Connected Client and ChatWindow.

--- ChatServer/src/client/Client.java
+++ ChatServer/src/client/Client.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
 
+import clientgui.ChatWindow;
+
 import message.Message;
 
 import common.Mailbox;
@@ -12,26 +14,16 @@ public class Client {
 
 	private Socket socket;
 
-	public Client(String host, int port) throws IOException {
+	public Client(String host, int port, Mailbox<Message> inbox,
+			Mailbox<Message> outbox) throws IOException {
 		InetAddress address = InetAddress.getByName(host);
 		socket = new Socket(address, port);
-		
-		Mailbox<Message> inbox = new Mailbox<Message>();
-		Mailbox<Message> outbox = new Mailbox<Message>();
-		
+
 		Thread t;
 		t = new ClientReadSocketThread(socket, inbox);
 		t.start();
 		t = new ClientWriteSocketThread(socket, outbox);
 		t.start();
-	}
-
-	public static void main(String[] args) {
-		try {
-			new Client("localhost", 1234);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 
 }
--- ChatServer/src/clientgui/ChatWindow.java
+++ ChatServer/src/clientgui/ChatWindow.java
@@ -1,6 +1,9 @@
 package clientgui;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -10,10 +13,34 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
+import client.Client;
+
+import message.Message;
+
+import common.Mailbox;
+
 public class ChatWindow extends JFrame {
+	
 	private static final long serialVersionUID = 1L;
+	
+	private Mailbox<Message> inbox;
+	private Mailbox<Message> outbox;
 
 	public ChatWindow() {
+		inbox = new Mailbox<Message>();
+		outbox = new Mailbox<Message>();
+		
+		initGUI();
+
+		try {
+			new Client("localhost", 1234, inbox, outbox);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void initGUI() {
 		setTitle("ChatServer: <server-name>"); // TODO Get the servers name
 //		setSize(800, 600);
 		
@@ -24,7 +51,7 @@ public class ChatWindow extends JFrame {
 		panel.setLayout(new BorderLayout(2, 2));
 		
 		String[] data = {"#abc", "#foo", "#bar", "#abc", "#foo", "#bar", "#abc", "#foo", "#bar", "#abc", "#foo", "#bar", "#abc", "#foo", "#bar", "#abc", "#foo", "#bar"};
-		JList<String> list = new JList<String>(data);
+		JList list = new JList(data);
 		JScrollPane scrollPane = new JScrollPane(list);
 		panel.add(scrollPane, BorderLayout.WEST);
 		
@@ -42,11 +69,25 @@ public class ChatWindow extends JFrame {
 		southPanel.add(button, BorderLayout.EAST);
 		
 		panel.add(southPanel, BorderLayout.SOUTH);
+		
+		ActionListener sendListener = new SendMessageListener();
+		textField.addActionListener(sendListener);
+		button.addActionListener(sendListener);
+		
+		setVisible(true);
+	}
+	
+	private class SendMessageListener implements ActionListener {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+		}
+		
 	}
 	
 	public static void main(String[] args) {
-		ChatWindow cw = new ChatWindow();
-		cw.setVisible(true);
+		new ChatWindow();
 	}
 
 }
