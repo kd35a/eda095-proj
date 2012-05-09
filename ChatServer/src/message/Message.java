944d425296756772889b3095a361b058c57a74b3
Carl Ekerot <carl.ekerot@lunicore.se>
2012-05-09 13:50:16 +0200

    Removed MessageFactory, moved method create out to Message. Done a test for Message.

--- ChatServer/src/message/Message.java
+++ ChatServer/src/message/Message.java
@@ -2,14 +2,21 @@ package message;
 
 import java.util.Map;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 public abstract class Message {
 	
 	protected String type;
 	protected Map<String, String> values;
 	
 	public String toJSON() {
-		/* TODO: Use JSON lib and do toString */
-		return null;
+		JSONObject msg = new JSONObject();
+		JSONObject attr = new JSONObject();
+		attr.putAll(values);
+		msg.put(type, attr);
+		return msg.toJSONString();
 	}
 	
 	public void setTime(String s) {
@@ -22,6 +29,41 @@ public abstract class Message {
 	
 	public void setValues(Map<String, String> values) {
 		this.values = values;
+	}
+	
+	public static Message fromJSON(String s) {
+		try {
+			JSONParser p = new JSONParser();
+			JSONObject msgObj = (JSONObject) p.parse(s);
+			if (msgObj.keySet().size() != 1)
+				throw new ParseException(ParseException.ERROR_UNEXPECTED_EXCEPTION);
+			String type = (String) msgObj.keySet().toArray()[0];
+
+			Message msg;
+			if (type.equals("pm"))
+				msg = new PrivateMessage();
+			else if (type.equals("cm"))
+				msg = new ChatroomMessage();
+			else if (type.equals("join"))
+				msg = new JoinMessage();
+			else if (type.equals("part"))
+				msg = new PartMessage();
+			else if (type.equals("connect"))
+				msg = new ConnectMessage();
+			else if (type.equals("disconnect"))
+				msg = new DisconnectMessage();
+			else if (type.equals("nick"))
+				msg = new NickMessage();
+			else
+				throw new ParseException(ParseException.ERROR_UNEXPECTED_TOKEN);
+			
+			JSONObject attr = (JSONObject) msgObj.get(type);
+			msg.setValues((Map<String, String>) attr);
+			return msg;
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 }
--- ChatServer/src/test/TestMessages.java
+++ ChatServer/src/test/TestMessages.java
@@ -1,6 +1,12 @@
 package test;
 
-import message.MessageFactory;
+import message.ChatroomMessage;
+import message.ConnectMessage;
+import message.DisconnectMessage;
+import message.JoinMessage;
+import message.Message;
+import message.NickMessage;
+import message.PartMessage;
 import message.PrivateMessage;
 
 import org.junit.Test;
@@ -11,10 +17,51 @@ public class TestMessages extends TestCase {
 	
 	@Test
 	public void testPM() {
-		PrivateMessage msg = (PrivateMessage) MessageFactory.create("{ \"pm\": { \"to\": \"u2\", \"msg\": \"hej\", \"time\": \"2012-05-09 11:35:22 +0200\" } }");
+		PrivateMessage msg = (PrivateMessage) Message.fromJSON("{ \"pm\": { \"to\": \"u2\", \"msg\": \"hej\", \"time\": \"2012-05-09 11:35:22 +0200\" } }");
 		assertEquals(msg.getMsg(), "hej");
 		assertEquals(msg.getTo(), "u2");
 		assertEquals(msg.getTime(), "2012-05-09 11:35:22 +0200");
+	}
+	
+	@Test
+	public void testCM() {
+		ChatroomMessage msg = (ChatroomMessage) Message.fromJSON("{ \"cm\": { \"room\": \"kryptoanarki\", \"msg\": \"hallå\", \"time\": \"2012-05-09 11:35:22 +0200\" } }");
+		assertEquals(msg.getMsg(), "hallå");
+		assertEquals(msg.getRoom(), "kryptoanarki");
+		assertEquals(msg.getTime(), "2012-05-09 11:35:22 +0200");
+	}
+	
+	@Test
+	public void testJoin() {
+		JoinMessage msg = (JoinMessage) Message.fromJSON("{ \"join\": { \"room\": \"kryptoanarki\", \"time\": \"2012-05-09 11:35:22 +0200\" } }");
+		assertEquals(msg.getRoom(), "kryptoanarki");
+		assertEquals(msg.getTime(), "2012-05-09 11:35:22 +0200");
+	}
+	
+	@Test
+	public void testPart() {
+		PartMessage msg = (PartMessage) Message.fromJSON("{ \"part\": { \"room\": \"kryptoanarki\", \"time\": \"2012-05-09 11:35:22 +0200\" } }");
+		assertEquals(msg.getRoom(), "kryptoanarki");
+		assertEquals(msg.getTime(), "2012-05-09 11:35:22 +0200");
+	}
+	
+	@Test
+	public void testConnect() {
+		ConnectMessage msg = (ConnectMessage) Message.fromJSON("{ \"connect\": { \"nick\": \"u\", \"time\": \"2012-05-09 11:35:22 +0200\" } }");
+		assertEquals(msg.getNick(), "u");
+		assertEquals(msg.getTime(), "2012-05-09 11:35:22 +0200");
+	}
+	
+	@Test
+	public void testDisonnect() {
+		DisconnectMessage msg = (DisconnectMessage) Message.fromJSON("{ \"disconnect\": { \"time\": \"2012-05-09 11:35:22 +0200\" } }");
+		assertEquals(msg.getTime(), "2012-05-09 11:35:22 +0200");
+	}
+	
+	@Test
+	public void testNick() {
+		NickMessage msg = (NickMessage) Message.fromJSON("{ \"nick\": { \"nick\": \"v\" } }");
+		assertEquals(msg.getNick(), "v");
 	}
 
 }
