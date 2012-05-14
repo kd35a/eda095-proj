ce682ba19bccb70ddc4df460daea4d2b4f1e8a5c
Fredrik Appelros <dt07fa4@student.lth.se>
2012-05-14 14:33:21 +0200

    Added close buttons for tabs. Sends PartMessage on closing.

--- ChatServer/src/clientgui/ChatWindow.java
+++ ChatServer/src/clientgui/ChatWindow.java
@@ -1,20 +1,31 @@
 package clientgui;
 
+import java.awt.BasicStroke;
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.swing.AbstractButton;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -29,17 +40,18 @@ import javax.swing.event.ChangeListener;
 
 import message.ChatroomMessage;
 import message.Message;
+import message.PartMessage;
 import message.PrivateMessage;
 import client.Client;
 
 public class ChatWindow extends JFrame implements ClientGUI, Observer {
-	
+
 	private static final long serialVersionUID = 1L;
 	private static final String PROGRAM_NAME = "ChatServer";
-	
+
 	private Map<String, ChatRoomPanel> chatrooms;
 	private Client client;
-	
+
 	/*
 	 * GUI components
 	 */
@@ -51,12 +63,12 @@ public class ChatWindow extends JFrame implements ClientGUI, Observer {
 	private JButton sendButton;
 	private JPanel southPanel;
 	private JMenuBar menuBar;
-	
+
 	public ChatWindow(Client client) {
 		this.client = client;
 		client.setChatWindow(this);
 		chatrooms = new HashMap<String, ChatRoomPanel>();
-		
+
 		initGUI();
 	}
 
@@ -76,7 +88,7 @@ public class ChatWindow extends JFrame implements ClientGUI, Observer {
 		mainPanel = new JPanel();
 		getContentPane().add(mainPanel);
 		mainPanel.setLayout(new BorderLayout(2, 2));
-		
+
 		participantsList = new JList(new String[0]);
 		scrollPane = new JScrollPane(participantsList);
 		mainPanel.add(scrollPane, BorderLayout.WEST);
@@ -86,7 +98,7 @@ public class ChatWindow extends JFrame implements ClientGUI, Observer {
 		mainPanel.add(tabbedPane, BorderLayout.CENTER);
 
 		southPanel = new JPanel(new BorderLayout(2, 2));
-		
+
 		messageInputField = new JTextField();
 		southPanel.add(messageInputField, BorderLayout.CENTER);
 
@@ -98,76 +110,173 @@ public class ChatWindow extends JFrame implements ClientGUI, Observer {
 		ActionListener sendListener = new SendMessageListener();
 		messageInputField.addActionListener(sendListener);
 		sendButton.addActionListener(sendListener);
-		
+
 		initMenu();
 
 		setLocationRelativeTo(null);
 		setVisible(true);
 	}
-	
+
 	private void joinChatroom(String name) {
 		ChatRoomPanel c = new ChatRoomPanel();
 		client.joinRoom(name);
 		chatrooms.put(name, c);
-		tabbedPane.addTab(name, c);
+		int index = tabbedPane.getTabCount();
+		tabbedPane.add(name, c);
+		tabbedPane.setTabComponentAt(index, new TabComponent(tabbedPane));
 	}
+
+	private class TabComponent extends JPanel {
+
+		private final JTabbedPane pane;
+
+		private TabComponent(final JTabbedPane pane) {
+			super(new FlowLayout(FlowLayout.LEFT, 0, 0));
+
+			this.pane = pane;
+
+			setOpaque(false);
+
+			JLabel label = new JLabel() {
+				public String getText() {
+					int i = pane.indexOfTabComponent(TabComponent.this);
+					if (i != -1) {
+						return pane.getTitleAt(i);
+					}
+					return null;
+				}
+			};
+			label.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 5));
+			add(label);
+
+			JButton button = new TabButton();
+			add(button);
+
+			setBorder(BorderFactory.createEmptyBorder(2, 0, 0, 0));
+		}
+
+		private class TabButton extends JButton implements ActionListener {
+
+			private TabButton() {
+				int size = 17;
+				setPreferredSize(new Dimension(size, size));
+
+				setContentAreaFilled(false);
+				setFocusable(false);
+				setBorder(BorderFactory.createEtchedBorder());
+				setBorderPainted(false);
+				addMouseListener(buttonMouseListener);
+				setRolloverEnabled(true);
+
+				addActionListener(this);
+			}
+
+			protected void paintComponent(Graphics g) {
+				super.paintComponent(g);
+				Graphics2D g2 = (Graphics2D) g.create();
+
+				g2.setStroke(new BasicStroke(2));
+				g2.setColor(Color.BLACK);
+
+				int delta = 6;
+				g2.drawLine(delta, delta, getWidth() - delta - 1, getHeight()
+						- delta - 1);
+				g2.drawLine(getWidth() - delta - 1, delta, delta, getHeight()
+						- delta - 1);
+				g2.dispose();
+			}
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String room = null;
+				int i = pane.indexOfTabComponent(TabComponent.this);
+				if (i != -1) {
+					room = pane.getTitleAt(i);
+					pane.remove(i);
+				}
+				PartMessage msg = new PartMessage();
+				msg.setRoom(room);
+				client.sendMessage(msg);
+			}
+
+		}
+
+	}
+
+	private final static MouseListener buttonMouseListener = new MouseAdapter() {
+		public void mouseEntered(MouseEvent e) {
+			Component component = e.getComponent();
+			if (component instanceof AbstractButton) {
+				AbstractButton button = (AbstractButton) component;
+				button.setBorderPainted(true);
+			}
+		}
+
+		public void mouseExited(MouseEvent e) {
+			Component component = e.getComponent();
+			if (component instanceof AbstractButton) {
+				AbstractButton button = (AbstractButton) component;
+				button.setBorderPainted(false);
+			}
+		}
+	};
 
 	private void initMenu() {
 		menuBar = new JMenuBar();
-		
+
 		JMenu fileMenu = new JMenu("File");
 		JMenuItem fileQuit = new JMenuItem("Quit");
 		fileQuit.addActionListener(new CloseActionListener(this));
 		fileMenu.add(fileQuit);
-		
+
 		JMenu serverMenu = new JMenu("Server");
-		
+
 		JMenuItem serverDisconnect = new JMenuItem("Disconnect from server");
 		serverDisconnect.setEnabled(false);
-		
+
 		JMenuItem serverJoinRoom = new JMenuItem("Join room...");
 		serverJoinRoom.addActionListener(new JoinRoomActionListener(this));
-		
+
 		serverMenu.add(serverDisconnect);
 		serverMenu.add(serverJoinRoom);
-		
+
 		menuBar.add(fileMenu);
 		menuBar.add(serverMenu);
 		setJMenuBar(menuBar);
 	}
 
 	private class SendMessageListener implements ActionListener {
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			String msg = messageInputField.getText();
 			messageInputField.setText("");
-			
+
 			int index = tabbedPane.getSelectedIndex();
-			if(index != -1) {
-				String room = tabbedPane.getTitleAt(index); 
-				
+			if (index != -1) {
+				String room = tabbedPane.getTitleAt(index);
+
 				ChatroomMessage m = new ChatroomMessage();
 				m.setMsg(msg);
 				m.setRoom(room);
-				
+
 				// putMessage(m);
-				
-				client.sendMessage(m);				
+
+				client.sendMessage(m);
 			}
 		}
-		
+
 	}
 
 	private class ChatWindowCloseListener extends WindowAdapter {
-		
+
 		public void windowClosing(WindowEvent e) {
 			System.out.println("Exit-button pressed");
 			client.disconnect();
 		}
-		
+
 	}
-	
+
 	/**
 	 * To be used by quit-buttons and -menu items. Just sends a Window Closing
 	 * event to window, and from there {@link ChatWindowCloseListener} takes
@@ -175,51 +284,53 @@ public class ChatWindow extends JFrame implements ClientGUI, Observer {
 	 */
 	private class CloseActionListener implements ActionListener {
 		private ChatWindow parent;
-		
+
 		public CloseActionListener(ChatWindow parent) {
 			this.parent = parent;
 		}
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			WindowEvent we = new WindowEvent(parent, WindowEvent.WINDOW_CLOSING);
 			Toolkit.getDefaultToolkit().getSystemEventQueue().postEvent(we);
 		}
-		
+
 	}
-	
+
 	private class JoinRoomActionListener implements ActionListener {
 		private ChatWindow parent;
-		
+
 		public JoinRoomActionListener(ChatWindow parent) {
 			this.parent = parent;
 		}
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			String room = JOptionPane.showInputDialog(parent, "Which room do"
-					+ " you want to join?",
-					"Join chatroom",
+					+ " you want to join?", "Join chatroom",
 					JOptionPane.PLAIN_MESSAGE);
 			room = room.trim();
 			if (room != null && !room.isEmpty()) {
 				joinChatroom(room);
 			}
 		}
-		
+
 	}
-	
+
 	private class TabChanged implements ChangeListener {
 
 		@Override
 		public void stateChanged(ChangeEvent ce) {
 			int index = tabbedPane.getSelectedIndex();
-			String room = tabbedPane.getTitleAt(index);
-			participantsList.setListData(client.getChatRoomParticipants(room));
+			if (index != -1) {
+				String room = tabbedPane.getTitleAt(index);
+				participantsList.setListData(client
+						.getChatRoomParticipants(room));
+			}
 		}
-		
+
 	}
-	
+
 	protected void sendMessage(Message msg) {
 		client.sendMessage(msg);
 	}
@@ -228,7 +339,7 @@ public class ChatWindow extends JFrame implements ClientGUI, Observer {
 	public void putMessage(ChatroomMessage msg) {
 		chatrooms.get(msg.getRoom()).putMessage(msg);
 	}
-	
+
 	public void repaint() {
 		super.repaint();
 		int index = tabbedPane.getSelectedIndex();
@@ -249,7 +360,7 @@ public class ChatWindow extends JFrame implements ClientGUI, Observer {
 		if (arg instanceof ChatroomMessage) {
 			putMessage((ChatroomMessage) arg);
 		} else if (arg instanceof PrivateMessage) {
-			
+
 		}
 	}
 
